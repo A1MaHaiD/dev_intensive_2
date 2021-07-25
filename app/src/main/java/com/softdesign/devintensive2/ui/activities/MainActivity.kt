6289fefb85c8bc9590f2d54f7e3ff4f6ca40c1e7
HanDroiD <super.lukianchuk2010@gmail.com>
2021-07-25 22:54:13 +0300

    again creating

---
+++ app/src/main/java/com/softdesign/devintensive2/ui/activities/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.softdesign.devintensive2.ui.activities
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.softdesign.devintensive2.databinding.ActivityMainBinding
+import com.softdesign.devintensive2.utils.ConstantManager
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = ConstantManager.TAG_PREFIX + "Main Activity"
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+}
---
+++ app/src/main/java/com/softdesign/devintensive2/utils/ConstantManager.kt
@@ -0,0 +1,5 @@
+package com.softdesign.devintensive2.utils
+
+object ConstantManager {
+    const val TAG_PREFIX="DEV_"
+}

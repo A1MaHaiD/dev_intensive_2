91f38da8ddd944fd423fe6be6117841ecdf5c250
HanDroiD <super.lukianchuk2010@gmail.com>
2021-08-04 10:37:52 +0300

    creating progress_splash.xml && style custom_dialog

--- app/src/main/java/com/softdesign/devintensive2/ui/activities/BaseActivity.kt
+++ app/src/main/java/com/softdesign/devintensive2/ui/activities/BaseActivity.kt
@@ -1,9 +1,12 @@
 package com.softdesign.devintensive2.ui.activities
 
 import android.app.ProgressDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.softdesign.devintensive2.R
 import com.softdesign.devintensive2.utils.ConstantManager
 import java.lang.Exception
 
@@ -11,8 +14,12 @@ class BaseActivity : AppCompatActivity() {
     private val TAG: String = ConstantManager.TAG_PREFIX + "BaseActivity"
     protected lateinit var mProgressDialog: ProgressDialog
     fun showProgress() {
-        if (mProgressDialog == null){
-            mProgressDialog = ProgressDialog(this)
+        if (mProgressDialog == null) {
+            mProgressDialog = ProgressDialog(this, R.style.custom_dialog)
+            mProgressDialog.setCancelable(false)
+            mProgressDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            mProgressDialog.show()
+            mProgressDialog.setContentView(R.layout.progress_splash)
         }
     }
 

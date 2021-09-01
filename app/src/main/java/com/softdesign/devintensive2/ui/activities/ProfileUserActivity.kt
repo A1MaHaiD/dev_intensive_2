8d802bbc11893a57b0d157ff1a220d4db3863285
HanDroiD <super.lukianchuk2010@gmail.com>
2021-09-01 13:59:43 +0200

    completed Task 6 DevIntensive

--- app/src/main/java/com/softdesign/devintensive2/ui/activities/ProfileUserActivity.kt
+++ app/src/main/java/com/softdesign/devintensive2/ui/activities/ProfileUserActivity.kt
@@ -1,9 +1,13 @@
 package com.softdesign.devintensive2.ui.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.ListView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.snackbar.Snackbar
 import com.softdesign.devintensive2.R
 import com.softdesign.devintensive2.data.storage.models.UserDTO
 import com.softdesign.devintensive2.databinding.ActivityProfileUserBinding
@@ -17,6 +21,7 @@ class ProfileUserActivity : BaseActivity() {
 
     private var mUserAbout: EditText? = null
     private var mCollapsingToolbarLayout: CollapsingToolbarLayout? = null
+    private var mCoordinatorLayout:CoordinatorLayout? = null
     private var mRepoListView: ListView? = null
 
     lateinit var binding: ActivityProfileUserBinding
@@ -34,6 +39,7 @@ class ProfileUserActivity : BaseActivity() {
         binding.tvUserInfoProjectProfile
         mUserAbout = findViewById(R.id.et_about)
         mCollapsingToolbarLayout = binding.ctlProfileUser
+        mCoordinatorLayout = binding.coordinatorLayoutProfileUser
 
         mRepoListView = findViewById(R.id.lv_repositories_list)
 
@@ -53,6 +59,22 @@ class ProfileUserActivity : BaseActivity() {
         val repositories: MutableList<String>? = userDTO?.repositories
         val repositoriesAdapter = RepositoriesAdapter(this, repositories)
         mRepoListView?.adapter = repositoriesAdapter
+
+        mRepoListView?.onItemClickListener = object :AdapterView.OnItemClickListener{
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+//                Snackbar.make(mCollapsingToolbarLayout!!,
+//                    "Репозиторій ${repositories!![position]}",
+//                    Snackbar.LENGTH_LONG)
+//                    .show()
+                //TODO: Реалізувати через Intent.ACTION_VIEW перегляд Репозиторія
+            }
+
+        }
 
         mUserAbout?.setText(userDTO?.about)
         binding.tvUserInfoRateProfile.text = userDTO?.rating
